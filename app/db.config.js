71c3041f70ba0748224bbc5e4b3c79eed6a2050f
Mai <angel.luck.0686@gmail.com>
2025-12-17 18:51:39 +0100

    ny

--- app/db.config.js
+++ app/db.config.js
@@ -1,28 +1,14 @@
-// module.exports = {
-//     HOST: process.env.MYSQLHOST || process.env.DB_HOST,
-//     USER: process.env.MYSQLUSER || process.env.DB_USER,
-//     PASSWORD: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
-//     DB: process.env.MYSQLDATABASE || process.env.DB_NAME,
-//     PORT: process.env.DB_PORT || 3306,
-//     dialect: "mysql",
-//     pool: {
-//         max: 5, 
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-// }
 module.exports = {
-  HOST: process.env.HOST,         // mysql91.unoeuro.com
-  USER: process.env.USER,         // mrln_dk
-  PASSWORD: process.env.PASSWORD, // cdknwyaehr5fzEtBFg43
-  DB: process.env.DB,             // mrln_dk_db
-  PORT: process.env.DB_PORT || 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-};
+    HOST: process.env.MYSQLHOST || process.env.DB_HOST,
+    USER: process.env.MYSQLUSER || process.env.DB_USER,
+    PASSWORD: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+    DB: process.env.MYSQLDATABASE || process.env.DB_NAME,
+    PORT: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+    pool: {
+        max: 5, 
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+}
--- app/models/index.js
+++ app/models/index.js
@@ -1,56 +1,32 @@
-// const dbConfig = require("../db.config.js");
-
-// const Sequelize = require("sequelize");
-// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-//   host: dbConfig.HOST,
-//   port: dbConfig.PORT,
-//   dialect: dbConfig.dialect,
-//   //operatorsAliases: false,
-
-//   pool: {
-//     max: dbConfig.pool.max,
-//     min: dbConfig.pool.min,
-//     acquire: dbConfig.pool.acquire,
-//     idle: dbConfig.pool.idle
-//   },
-//   dialectOptions: {
-//     ssl: {
-//       require: false,
-//       rejectUnauthorized: false
-//     }
-//   },
-//   logging: console.log
-// });
-
-// const db = {};
-
-// db.Sequelize = Sequelize;
-// db.sequelize = sequelize;
-
-// db.products = require("./product.model.js")(sequelize, Sequelize);
-// db.users = require("./user.model.js")(sequelize, Sequelize);
-
-// module.exports = db;
-
 const dbConfig = require("../db.config.js");
-const Sequelize = require("sequelize");
 
+const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   port: dbConfig.PORT,
   dialect: dbConfig.dialect,
-  pool: dbConfig.pool,
+  //operatorsAliases: false,
+
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  },
   dialectOptions: {
-    ssl: false  // Simply.com understøtter normalt ikke SSL
+    ssl: {
+      require: false,
+      rejectUnauthorized: false
+    }
   },
   logging: console.log
 });
 
 const db = {};
+
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-// Models
 db.products = require("./product.model.js")(sequelize, Sequelize);
 db.users = require("./user.model.js")(sequelize, Sequelize);
 
